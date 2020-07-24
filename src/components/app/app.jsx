266bdab67666f2a03f75fc2fdc0117fa44f518ce
Egor <egor@serverice.ru>
2020-07-24 15:27:10 +0700

    WIP

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,18 +1,102 @@
-import React from "react";
+import React, {PureComponent} from "react";
 import PropTypes from 'prop-types';
 import WelcomeScreen from "../welcome-screen/welcome-screen.jsx";
+import {Switch, Route, BrowserRouter} from "react-router-dom";
+import ArtistQuestionScreen from "../artist-question-screen/artist-question-screen.jsx";
+import GenreQuestionScreen from "../genre-question-screen/genre-question-screen.jsx";
+import {GameType} from "../../const.js";
 
-const App = (props) => {
-  const {errors, onWelcomeButtonClick} = props;
-  return <WelcomeScreen
-    errors = {errors}
-    onWelcomeButtonClick = {onWelcomeButtonClick}
-  />;
-};
+class App extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      step: -1,
+    };
+  }
+
+  _renderGameScreen() {
+    const {errors, questions} = this.props;
+    const {step} = this.state;
+    const question = questions[step];
+
+    if (step === -1 || step >= questions.length) {
+      return (
+        <WelcomeScreen
+          errorsCount={errors}
+          onWelcomeButtonClick={() => {
+            this.setState({
+              step: 0,
+            });
+          }}
+        />
+      );
+    }
+
+    if (question) {
+      switch (question.type) {
+        case GameType.ARTIST:
+          return (
+            <ArtistQuestionScreen
+              question={question}
+              onAnswer={() => {
+                this.setState((prevState) => ({
+                  step: prevState.step + 1,
+                }));
+              }}
+            />
+          );
+        case GameType.GENRE:
+          return (
+            <GenreQuestionScreen
+              question={question}
+              onAnswer={() => {
+                this.setState((prevState) => ({
+                  step: prevState.step + 1,
+                }));
+              }}
+            />
+          );
+      }
+    }
+    return null;
+  }
+
+  render() {
+    const {questions} = this.props;
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            {this._renderGameScreen()}
+          </Route>
+          <Route exact path="/artist">
+            <ArtistQuestionScreen
+              question={questions[1]}
+              onAnswer={() => {}}
+            />
+          </Route>
+          <Route exact path="/genre">
+            <GenreQuestionScreen
+              question={questions[0]}
+              onAnswer={() => {}}
+            />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+
+}
+
+// return <GuessArtist
+//   errors = {errors}
+//   onWelcomeButtonClick = {onWelcomeButtonClick}
+// />;
 
 App.propTypes = {
   errors: PropTypes.number.isRequired,
-  onWelcomeButtonClick: PropTypes.func.isRequired,
+  questions: PropTypes.array.isRequired,
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app/app.jsx";
+import questions from "./mocks/questions.js";
 
 const settings = {
   errorsCount: 4,
@@ -11,6 +12,7 @@ ReactDOM.render(
     <App
       errors = {settings.errorsCount}
       onWelcomeButtonClick = {settings.onWelcomeButtonClick}
+      questions={questions}
     />,
     document.querySelector(`#root`)
 );
