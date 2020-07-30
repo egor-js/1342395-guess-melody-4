eeb37544823af8befe5fa548f4dd9b941127b627
Egor <egor@serverice.ru>
2020-07-30 19:37:24 +0700

    add component game-screen

--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -2,43 +2,6 @@ import React from "react";
 import renderer from "react-test-renderer";
 import App from "./app";
 
-const AVATAR_URL = `https://api.adorable.io/avatars/128`;
-
-const questions = [
-  {
-    type: `artist`,
-    song: {
-      artist: `Jim Beam`,
-      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
-    },
-    answers: [{
-      picture: `${AVATAR_URL}/${Math.random()}`,
-      artist: `John Snow`,
-    }, {
-      picture: `${AVATAR_URL}/${Math.random()}`,
-      artist: `Jack Daniels`,
-    }, {
-      picture: `${AVATAR_URL}/${Math.random()}`,
-      artist: `Jim Beam`,
-    }],
-  }, {
-    type: `genre`,
-    genre: `rock`,
-    answers: [{
-      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
-      genre: `rock`,
-    }, {
-      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
-      genre: `blues`,
-    }, {
-      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
-      genre: `jazz`,
-    }, {
-      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
-      genre: `rock`,
-    }],
-  }];
-
 const errors = 4;
 const onWelcomeButtonClick = function () {};
 
